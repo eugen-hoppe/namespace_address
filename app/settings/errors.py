1009d6cdd16d048aa09dcd5fd775ab43f0351098
Eugen <eugen.hoppe.de@gmail.com>
2023-12-03 09:21:46 +0100

    test commit

--- app/settings/errors.py
+++ app/settings/errors.py
@@ -2,6 +2,9 @@ from enum import Enum
 
 
 class ErrorSnippet(str, Enum):
+    """
+    ErrorSnippet
+    """
     DOMAIN: str = "Slug is not valid for domain: "
     VERSION: str = "Slug is not valid for version: "
 
